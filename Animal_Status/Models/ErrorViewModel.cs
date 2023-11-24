23a14fdb1a41a30eeb58e0d2f7eda07ab4496854
buloc <buloc@CLOAKER>
2023-11-24 10:44:22 +0300

    Добавьте файлы проекта.

---
+++ Animal_Status/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Animal_Status.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
