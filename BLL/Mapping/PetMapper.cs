4df38883016f5d612219a6fa4f1f750d17ec0861
Сергей Свирский <bulochka5544@gmail.com>
2023-11-28 00:41:31 +0300

    Добавил маперы

---
+++ BLL/Mapping/PetMapper.cs
@@ -0,0 +1,14 @@
+﻿using Animal_Status;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Mapping
+{
+    public class PetMapper : EntityMapper<Pet, PetDTO>
+
+    {
+    }
+}
